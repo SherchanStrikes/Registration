80317c5b7422cc93f840c11a840dd3f569accd02
SherchanStrikes <saphal@outlook.sg>
2022-01-01 15:54:06 +0545

    alert dialog

--- lib/screen/toastExam.dart
+++ lib/screen/toastExam.dart
@@ -9,10 +9,37 @@ class toast extends StatefulWidget {
 }
 
 class _toastState extends State<toast> {
+
   TextEditingController user = TextEditingController();
   TextEditingController pw = TextEditingController();
-
+  String result = "";
   final _formkey = GlobalKey<FormState>();
+
+  showAlertDialog(BuildContext context) {
+    Widget okbtn = ElevatedButton(onPressed: () {
+      Navigator.of(context).pop();
+
+      Fluttertoast.showToast(msg: "Goodbye!!",gravity: ToastGravity.CENTER,backgroundColor: Colors.red);
+    },
+      child: const Text("OK"),);
+    Widget cnclbtn = ElevatedButton(onPressed: () {
+      Navigator.of(context).pop();
+    }, child: const Text("Cancel"));
+    AlertDialog alert = AlertDialog(
+      backgroundColor: Colors.teal,
+      elevation: 20.0,
+
+      title: const Text("Alert Dialog"),
+      content: const Text("Succesfully logged in!"),
+      actions: [
+        okbtn,
+        cnclbtn
+      ],
+    );
+    showDialog(context: context, builder: (BuildContext context){
+      return alert;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,84 +81,95 @@ class _toastState extends State<toast> {
       body: SingleChildScrollView(
         child: SafeArea(
             child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Username must be filled!!";
-                    }
-                    return null;
-                  },
-                  controller: user,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your username',
-                    labelText: 'Username',
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Password must be filled!!";
-                    }
-                    return null;
-                  },
-                  controller: pw,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                      hintText: "Enter your password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formkey.currentState!.validate()) {
-                      setState(() {
-
-                      });
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Something Wrong!')));
-                    }
-                  },
-                  child: const Text('Signup'),
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+              padding: const EdgeInsets.all(30.0),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Username must be filled!!";
+                        }
+                        return null;
+                      },
+                      controller: user,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your username',
+                        labelText: 'Username',
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:[
-                    const Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.purple,
-                    fontStyle: FontStyle.italic,
-
-                  ),
-                ),
-                  ElevatedButton(onPressed: (){}, child: const Text("Login"))
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password must be filled!!";
+                        }
+                        return null;
+                      },
+                      controller: pw,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                          hintText: "Enter your password",
+                          labelText: "Password"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            Fluttertoast.showToast(
+                                msg: "Signed Up!!",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.green,
+                                textColor: Colors.black12);
+                          });
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text(
+                                  'Something Wrong!')));
+                        }
+                      },
+                      child: const Text('Signup'),
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Already have an account?",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.purple,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              showAlertDialog(context);
+                            },
+                            child: const Text("Login"))
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          ),
-        )),
+                ),
+              ),
+            )),
       ),
     );
   }
